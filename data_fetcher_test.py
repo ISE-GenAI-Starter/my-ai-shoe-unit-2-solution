745639d5ef2c2c07e1983ba9809029a29a72ee24
NyW8 <tobluee@gmail.com>
2024-07-22 21:38:46 -0700

    Update starter code

--- data_fetcher_test.py
+++ data_fetcher_test.py
@@ -1,9 +1,19 @@
-## data_fetcher_test.py
-##
-## Created for TechExchange 2025, Software Development Studio with Google
-##
-## This file contains tests for data_fetcher.py.
-##
-## You will write these tests in Unit 3.
+#############################################################################
+# data_fetcher_test.py
+#
+# Created for TechExchange 2025, Software Development Studio with Google
+#
+# This file contains tests for data_fetcher.py.
+#
+# You will write these tests in Unit 3.
+#############################################################################
+import unittest
 
-# TODO(nyahway): Write a template for the test file.
+class TestDataFetcher(unittest.TestCase):
+
+    def test_foo(self):
+        """Tests foo."""
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
